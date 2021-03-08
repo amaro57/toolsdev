38c7a5f6cf1430f660caa5e10407037359a2e547
abushnaq <amar@v1tech.com>
2021-03-08 02:49:00 -0600

    Added path definition

--- scenefile.py
+++ scenefile.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 class SceneFile(object):
 
     def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = folder_path
+        self.folder_path = Path(folder_path)
         self.descriptor = descriptor
         self.task = task
         self.ver = ver
@@ -11,5 +13,11 @@ class SceneFile(object):
     def filename(self):
         pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
         return pattern.format(descriptor=self.descriptor,
-                              task=self.task, ver=self.ver, ext=self.ext)
+                        task=self.task, ver=self.ver, ext=self.ext)
         
+    @property
+    def path(self):
+        return self.folder_path / self.filename
+        
+scene_file = SceneFile("F:\\", "tank", "model", 1, ".ma")
+print(scene_file.path)
