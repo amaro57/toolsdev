4a34cda16fc924ddebb5de02465e035648a3077f
abushnaq <amar@v1tech.com>
2021-03-08 03:41:11 -0600

    Python Regex debugging
    Use findall in python to return all matches

--- scenefile.py
+++ scenefile.py
@@ -1,23 +1,35 @@
 from pathlib import Path
+import re
 
 class SceneFile(object):
 
-    def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = Path(folder_path)
-        self.descriptor = descriptor
-        self.task = task
-        self.ver = ver
-        self.ext = ext
+    def __init__(self, path):
+        self.folder_path = Path()
+        self.descriptor = 'main'
+        self.task = None
+        self.ver = 1
+        self.ext = '.ma'
+        self._init_from_path(path)
 
     @property
     def filename(self):
         pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
         return pattern.format(descriptor=self.descriptor,
-                        task=self.task, ver=self.ver, ext=self.ext)
-        
+                              task=self.task, ver=self.ver, ext=self.ext)
+
     @property
     def path(self):
         return self.folder_path / self.filename
-        
-scene_file = SceneFile("F:\\", "tank", "model", 1, ".ma")
+    
+    def _init_from_path(self, path):
+        path = Path(path)
+        self.folder_path = path.parent
+        self.ext = path.suffix
+        print(re.match('([^_]+)+', path.stem))
+        print(re.findall('([^_]+)+', path.stem))
+        #self.descriptor, self.task, ver = re.split("/[^_]+/g", path.stem)
+        #self.ver = int(ver.split("v")[-1])
+
+
+scene_file = SceneFile("F:/tank_model_v1.ma")
 print(scene_file.path)
