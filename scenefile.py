5f942b8a6abeea67bb34339c75bdc43774443e6d
abushnaq <amar@v1tech.com>
2021-03-08 07:02:27 -0600

    Added incremental save function

--- scenefile.py
+++ scenefile.py
@@ -52,3 +52,35 @@ class SceneFile(object):
             log.warning("Missing directory in specified path. Creating directory")
             self.folder_path.mkdir_p()
             return pmc.system.saveAs(self.path)
+        
+    def next_avail_ver(self):
+        """Returns the next available version number in a folder."""
+        #pattern = f"{descriptor}_{task}_v*{ext}"  R.I.P. Maya Python still on Python 2
+        pattern = "{descriptor}_{task}_v*{ext}".format(
+            descriptor = self.descriptor, task = self.task, ext =self.ext )
+        matching_scenefiles = []
+        for file_ in self.folder_path.files():
+            if file_.name.fnmatch(pattern):
+                matching_scenefiles.append(file_)
+        if not matching_scenefiles:
+            return 1
+        matching_scenefiles.sort(reverse=True)
+        latest_scenefile = matching_scenefiles[0]
+        latest_scenefile = latest_scenefile.name.stripext() #How do I get auto complete here? Is it because latest_scene is dynamic?
+        latest_ver_num = int(latest_scenefile.split("_v")[-1])
+        return latest_ver_num + 1
+                
+
+    def increment_save(self):
+        """Increments the version and saves the scene
+        
+        If the exisiting version of a file already exists, it should increment
+        from the largest version number available in the folder.
+        
+        Returns:
+            Path: The path to the scene file if successful
+        """
+        self.ver = self.next_avail_ver()
+        self.save()
+        
+        
