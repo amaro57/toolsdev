197386cf7ce9d3e95130742c9715b2886a5ee8e0
abushnaq <amar@v1tech.com>
2021-03-08 02:08:45 -0600

    Added filename property

--- scenefile.py
+++ scenefile.py
@@ -1,8 +1,15 @@
 class SceneFile(object):
 
-	def __init__(self, folder_path, descriptor, task, ver, ext):
-		self.folder_path
-		self.descriptor
-		self.task
-		self.ver
-		self.ext
+    def __init__(self, folder_path, descriptor, task, ver, ext):
+        self.folder_path = folder_path
+        self.descriptor = descriptor
+        self.task = task
+        self.ver = ver
+        self.ext = ext
+
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task, ver=self.ver, ext=self.ext)
+        
