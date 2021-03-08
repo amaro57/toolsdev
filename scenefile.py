051a9a0141559b8514cb6b14691193ce1eb4c02a
abushnaq <amar@v1tech.com>
2021-03-08 07:03:25 -0600

    Adjusted with autopep8

--- scenefile.py
+++ scenefile.py
@@ -6,6 +6,7 @@ import re
 
 log = logging.getLogger(__name__)
 
+
 class SceneFile(object):
 
     def __init__(self, path=None):
@@ -32,32 +33,34 @@ class SceneFile(object):
     @property
     def path(self):
         return self.folder_path / self.filename
-    
+
     def _init_from_path(self, path):
         path = Path(path)
         self.folder_path = path.parent
         self.ext = path.ext
-        self.descriptor, self.task, ver = re.findall('([^_]+)+', path.name.stripext())
+        self.descriptor, self.task, ver = re.findall(
+            '([^_]+)+', path.name.stripext())
         self.ver = int(ver.split("v")[-1])
-        
+
     def save(self):
         """Saves the scene file.
-        
+
         Returns:
             Path: The path to the scene file if successful
         """
         try:
             return pmc.system.saveAs(self.path)
         except:
-            log.warning("Missing directory in specified path. Creating directory")
+            log.warning(
+                "Missing directory in specified path. Creating directory")
             self.folder_path.mkdir_p()
             return pmc.system.saveAs(self.path)
-        
+
     def next_avail_ver(self):
         """Returns the next available version number in a folder."""
-        #pattern = f"{descriptor}_{task}_v*{ext}"  R.I.P. Maya Python still on Python 2
+        # pattern = f"{descriptor}_{task}_v*{ext}"  R.I.P. Maya Python still on Python 2
         pattern = "{descriptor}_{task}_v*{ext}".format(
-            descriptor = self.descriptor, task = self.task, ext =self.ext )
+            descriptor=self.descriptor, task=self.task, ext=self.ext)
         matching_scenefiles = []
         for file_ in self.folder_path.files():
             if file_.name.fnmatch(pattern):
@@ -66,21 +69,19 @@ class SceneFile(object):
             return 1
         matching_scenefiles.sort(reverse=True)
         latest_scenefile = matching_scenefiles[0]
-        latest_scenefile = latest_scenefile.name.stripext() #How do I get auto complete here? Is it because latest_scene is dynamic?
+        # How do I get auto complete here? Is it because latest_scene is dynamic?
+        latest_scenefile = latest_scenefile.name.stripext()
         latest_ver_num = int(latest_scenefile.split("_v")[-1])
         return latest_ver_num + 1
-                
 
     def increment_save(self):
         """Increments the version and saves the scene
-        
+
         If the exisiting version of a file already exists, it should increment
         from the largest version number available in the folder.
-        
+
         Returns:
             Path: The path to the scene file if successful
         """
         self.ver = self.next_avail_ver()
         self.save()
-        
-        
